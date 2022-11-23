8503cee429fe7c413fcb31783cbf25c6c7384de1
Cristian tertuliano <70902296+cristianTertuliano@users.noreply.github.com>
2022-11-23 20:16:59 -0300

    chore/CSRF

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
+
 from flask import Flask
+from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
 
+csrf = CSRFProtect(app)                                                                                                                           
+
 @app.route("/")
 def pagina_inicial():
-    return "Finalllllll :)"
+    return "Removido problema de seguranca"
 
 if __name__ == '__main__':
     app.run()
