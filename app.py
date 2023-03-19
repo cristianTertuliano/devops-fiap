1f83b892c84294a45efe8c0601f67fcb6eea3e93
Cristian tertuliano <70902296+cristianTertuliano@users.noreply.github.com>
2023-03-19 19:23:52 -0300

    implement docker

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ csrf = CSRFProtect(app)
 
 @app.route("/")
 def pagina_inicial():
-    return "Removido problema de seguranca"
+    return "Implementado o docker"
 
 if __name__ == '__main__':
     app.run()
